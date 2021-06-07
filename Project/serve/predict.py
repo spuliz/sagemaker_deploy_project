4f2e4f6ec0f2d21f8ae93524a2f99c8a6a7d4dd1
EC2 Default User <ec2-user@ip-172-16-80-77.ec2.internal>
2021-06-07 16:40:26 +0000

    project completed

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -72,6 +72,8 @@ def predict_fn(input_data, model):
 
     data_X = None
     data_len = None
+    words = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -88,5 +90,11 @@ def predict_fn(input_data, model):
     #       be a numpy array which contains a single integer which is either 1 or 0
 
     result = None
+    with torch.no_grad():
+        #output = predictor.predict(data)
+        output = model.forward(data)
+    
+    #output = output.cpu()
+    result = int(np.round(output.numpy()))
 
     return result
--- Project/train/train.py
+++ Project/train/train.py
@@ -68,8 +68,24 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     """
     
     # TODO: Paste the train() method developed in the notebook here.
-
-    pass
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: Complete this train method to train the model provided.
+            optimizer.zero_grad() # zero the parameter gradients
+            # forward + backward + optimize
+            output = model.forward(batch_X) #compute model output
+            loss = loss_fn(output, batch_y) #calculate loss
+            loss.backward() # compute gradients of all variables with respect to loss
+            optimizer.step() #perform updates using calculated gradients
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 
 if __name__ == '__main__':
